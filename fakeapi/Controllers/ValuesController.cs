014c2f8f9a476b0f007b7efae1abb4076d0d24f2
Bruno Sena e Silva Correia <brunossc@outlook.com>
2019-06-25 19:50:16 -0300

    first commit

---
+++ fakeapi/Controllers/ValuesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+
+namespace fakeapi.Controllers
+{
+    [EnableCors("AllowCors")]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static List<Video> fakeVideoTable;
+
+        public ValuesController()
+        {
+            if (fakeVideoTable == null)
+            {
+                fakeVideoTable = new List<Video>();                
+            }
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Video>> Get()
+        {
+            return fakeVideoTable;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Video> Get(int id)
+        {
+            return fakeVideoTable.Where(x=>x.Id == id).FirstOrDefault();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public Video Post(Video video)
+        {
+            if (video != null &&
+            !String.IsNullOrEmpty(video.Title) &&
+            !String.IsNullOrEmpty(video.Description))
+            {
+                video.Id = fakeVideoTable.Count()+1;
+                fakeVideoTable.Add(video);
+                return video;
+            }            
+
+            return null;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public Video Put(int id, Video video)
+        {
+            Video _vi = fakeVideoTable.Where(x=>x.Id == id).FirstOrDefault();
+            _vi.Title = video.Title;
+            _vi.Description = video.Description;
+            return _vi;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            fakeVideoTable.Remove(fakeVideoTable.Where(x=>x.Id == id).FirstOrDefault());
+        }
+    }
+}
---
+++ fakeapi/Dto/video.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Video
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string UserId { get; set; }
+}
